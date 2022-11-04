e7896a4326253b118d5afaa326d1ef77815f1efd
erwaen <erikwasmosy98@gmail.com>
2022-11-04 00:48:43 -0300

    add the body of the ant, only spheres

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ const renderer = new THREE.WebGLRenderer({
 
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
-camera.position.setZ(30);
+camera.position.setZ(80);
 
 const controls = new OrbitControls(camera, renderer.domElement);
 
@@ -21,6 +21,7 @@ const ambientLight = new THREE.AmbientLight(0xffffff);
 const gridHelper = new THREE.GridHelper(200, 50);
 
 scene.add(gridHelper, ambientLight);
+
 
 function createTorus(){
   const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
@@ -31,6 +32,54 @@ function createTorus(){
 
 const torus = createTorus();
 scene.add(torus);
+
+// Create the ant system or like a invisible box that contain de ANT
+const antSystem = new THREE.Object3D();
+antSystem.position.y = 1.5;
+scene.add(antSystem);
+
+const texture = new THREE.TextureLoader().load('dark-concrete-texture-background.jpg');
+
+// ################### MID PART OF THE BODY (SPEHERE and Legs)###################
+// body mid mesh
+const midBodyMesh = new THREE.Object3D(); // only the mesh
+antSystem.add(midBodyMesh);
+
+
+// sphere of the body 
+let geometry = new THREE.SphereGeometry(1, 32, 32);
+let material = new THREE.MeshStandardMaterial({color: 0xfffffff, map: texture});
+const antBodyMid = new THREE.Mesh(geometry, material);
+midBodyMesh.add(antBodyMid);
+
+// TODO: Create the legs of the ant
+
+
+// ################### HEAD OF THE BODY (SPHERE y Antenas)###################
+const headAntMesh = new THREE.Object3D();
+headAntMesh.position.x = 1.1;
+headAntMesh.position.y = 0.2;
+antSystem.add(headAntMesh);
+
+geometry = new THREE.SphereGeometry(1.05, 32, 32);
+material = new THREE.MeshStandardMaterial({color: 0xfffffff, map: texture});
+const antHead = new THREE.Mesh(geometry, material);
+headAntMesh.add(antHead);
+
+// TODO: ANTENA
+
+
+// ################### BACK PART OF THE BODY (SPHERE Only, no mesh necessary)###################
+geometry = new THREE.SphereGeometry(1.4, 32, 32);
+material = new THREE.MeshStandardMaterial({color: 0xfffffff, map: texture});
+const antBack = new THREE.Mesh(geometry, material);
+antBack.position.x = -1.6;
+antBack.position.y = 0.4;
+//antBack.position.y = 0.2
+antSystem.add(antBack);
+
+
+
 
 function animate(){
   requestAnimationFrame(animate);
