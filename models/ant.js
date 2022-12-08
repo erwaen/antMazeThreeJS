3bab33d720094d3d67c755930629c56157cae040
erwaen <erikwasmosy98@gmail.com>
2022-12-08 15:39:28 -0300

    add sugar model

--- models/ant.js
+++ models/ant.js
@@ -1,6 +1,5 @@
 import * as THREE from 'three';
 
-import { MeshLine, MeshLineMaterial, MeshLineRaycast } from 'three.meshline';
 // ant model
 class Ant {
     constructor() {
---
+++ models/sugar.js
@@ -0,0 +1,14 @@
+import * as THREE from 'three';
+
+class Sugar{
+    constructor(){
+        this.geometry = new THREE.ConeGeometry( 1, 0.5, 9 );
+        this.material = new THREE.MeshBasicMaterial( {color: 0xffffff} );
+        this.mesh = new THREE.Mesh(this.geometry, this.material);
+
+        const [x, y, z] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread(150));
+        this.mesh.position.set(x,0.2,z);
+    }
+}
+
+export default Sugar;
